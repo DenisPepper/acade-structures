9dd1717461dac603fdd86401cf1114125b433b7f
DenisPepper <thech@yandex.ru>
2023-09-29 10:09:27 +0300

    Реализует метод isEmpty()

--- stack-by-linked-list.ts
+++ stack-by-linked-list.ts
@@ -2,6 +2,7 @@ import { LinkedList } from './linked-list';
 
 interface IStack {
   items: LinkedList;
+  readonly length: number;
   push(value: any): void;
   pop(): any;
   isEmpty(): boolean;
@@ -10,17 +11,29 @@ interface IStack {
 
 export class Stack implements IStack {
   items: LinkedList;
+  length: number;
 
   constructor() {
     this.items = new LinkedList();
+    this.length = 0;
   }
 
   push(value: any): void {
     this.items.addFirst(value);
+    this.length += 1;
   }
 
   pop() {
+    if (this.length === 0) {
+      return;
+    }
+
+    this.length -= 1;
     return this.items.removeFirst();
+  }
+
+  isEmpty(): boolean {
+    return this.length === 0;
   }
 
   toArray(): any[] {
