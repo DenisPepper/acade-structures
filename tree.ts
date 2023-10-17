08161f9856e1c6b8430c2968baf53b6e8a0aeebb
DenisPepper <thech@yandex.ru>
2023-10-17 18:33:41 +0300

    Имплементирует интерфейс узла дерева

--- tree.ts
+++ tree.ts
@@ -5,7 +5,29 @@ interface INode {
   removeNode(index: number): INode | null;
 }
 
-class Node implements INode {}
+class Node implements INode {
+  children: INode[];
+  parent: INode | null;
+
+  constructor(parent = null) {
+    this.parent = parent;
+    this.children = [];
+  }
+
+  addNode(node: INode): void {
+    this.children.push(node);
+  }
+
+  removeNode(index: number): INode | null {
+    let node: INode | null = null;
+    this.children = this.children.filter((el, i) => {
+      if (i === index) node = el;
+      return i !== index;
+    });
+
+    return node;
+  }
+}
 
 export class Tree {
   root: Node;
