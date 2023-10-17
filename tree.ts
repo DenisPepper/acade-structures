0fc8af20568e4a1318d755d7a0ae3954f1a8eebb
DenisPepper <thech@yandex.ru>
2023-10-17 19:18:27 +0300

    Добавит примеры использования

--- tree.ts
+++ tree.ts
@@ -52,4 +52,7 @@ export class Tree {
 const tree = new Tree('Кровать КМ-1');
 const pack1 = tree.root.addNode('пакет 1/2');
 const pack2 = tree.root.addNode('пакет 2/2');
-console.log(pack1, pack2);
+const bl = pack1.node?.addNode('Боковина левая');
+const br = pack1.node?.addNode('Боковина правая');
+const iz = pack2.node?.addNode('Изголовье');
+console.dir(tree);
